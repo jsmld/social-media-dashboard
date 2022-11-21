0c3084505c4e057d691e40621413396220f88984
jsmld <samvanerpersroijaards@gmail.com>
2022-11-21 16:03:50 -0300

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App: React.FC = () => (
+	<div>
+      Social Media Dashboard
+	</div>
+);
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import App from './App';
+import theme from './themes/theme';
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+	<React.StrictMode>
+		<ThemeProvider theme={theme}>
+			{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+			<CssBaseline />
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>
+);
