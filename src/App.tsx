d5905cd5b41525ae95b28fb5ff5389aa0e0f3729
jsmld <samvanerpersroijaards@gmail.com>
2022-11-22 13:38:05 -0300

    Implements Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Box, Container } from '@mui/material';
+import Header from './components/Header';
 
 const App: React.FC = () => (
-	<div>
-      Social Media Dashboard
-	</div>
+	<Box bgcolor='#F8F9FF'>
+		<Container maxWidth='xl'>
+			<Header />
+		</Container>
+	</Box>
 );
 
 export default App;
